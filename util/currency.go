1ccfe1f5915355acd5992e4a718e65f6fb1ba01c
Deepanshu Mehra <deepanshumehra2102@gmail.com>
2024-11-26 12:18:07 +0530

    feat : added custom validator for currency

---
+++ util/currency.go
@@ -0,0 +1,16 @@
+package util
+
+const (
+	USD = "USD"
+	INR = "INR"
+	EUR = "EUR"
+)
+
+func IsSupportedCurrency(currency string) bool {
+	switch currency {
+	case USD, EUR, INR:
+		return true
+	default:
+		return false
+	}
+}
